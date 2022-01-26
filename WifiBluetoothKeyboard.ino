2e4e61219022200776f9f5608c98df32439d1c66
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-01-25 20:06:52 -0800

    cleaned up

--- WifiBluetoothKeyboard.ino
+++ WifiBluetoothKeyboard.ino
@@ -3,10 +3,9 @@
 #include <WiFiClient.h>
 #include <WiFiAP.h>
 
-#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+#define LED_BUILTIN 2   
 #define USING_CORS_FEATURE true
 
-// Set these to your desired credentials.
 const char *ssid = "WifiKeyboard";
 const char *password = "mySecretPassword";
 
@@ -15,69 +14,38 @@ BleKeyboard bleKeyboard;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-
   //  Serial.begin(115200);
-  //  Serial.println();
-  //  Serial.println("Configuring access point...");
-
-  // You can remove the password parameter if you want the AP to be open.
   WiFi.softAP(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
-  //  Serial.print("AP IP address: ");
-  //  Serial.println(myIP);
-  // server.enableCORS(true);
   server.begin();
-
-  //  Serial.println("Server started");
   bleKeyboard.begin();
-  //  Serial.println("Bluetooth started");
 }
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {                             // if you get a client,
+  WiFiClient client = server.available();   
+  if (client) {                             
     digitalWrite(LED_BUILTIN, HIGH);
-    //    Serial.println("New Client.");           // print a message out the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+    String currentLine = "";                
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        if (c == '\n') {                    
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Access-Control-Allow-Origin: *");
             client.println("Connection: close");
-
-            //            client.println();
-            //            // the content of the HTTP response follows the header:
-            //            client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-            //            client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-
-            // The HTTP response ends with another blank line:
             client.println();
-            // break out of the while loop:
             digitalWrite(LED_BUILTIN, LOW);
             break;
-          } else {    // if you got a newline, then clear currentLine:
+          } else {    
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {  
+          currentLine += c;      
         }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-
-        //          digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-        //          digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-        //        Serial.println(currentLine);
+        
         if (currentLine.endsWith("GET /ret/EF")) {
           bleKeyboard.write(KEY_RETURN);
         } else if (currentLine.endsWith("GET /bk/EF")) {
