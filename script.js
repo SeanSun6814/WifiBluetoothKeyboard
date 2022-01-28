991c14d590dcda0c90c15b924a1bc22a5c7e8e1f
SeanSun6814 <33432158+SeanSun6814@users.noreply.github.com>
2022-01-27 16:41:09 -0800

    textdisplay working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let inputbox, serverIpInputbox, statusBox, avgSpeedBox, speedometerProgressbar, queueSizeBox;
+let inputbox, serverIpInputbox, statusBox, avgSpeedBox, speedometerProgressbar, queueSizeBox, displayTextBox;
 let modifierKeyDown = { "Shift": false, "Alt": false, "Control": false };
 let sendQueue = [];
-let sleeping = true;
+let sleeping = true, listening = false, connected = false, requestListening = true;
 let avgSpeed = 0, requestCount = 0, avgSpeedStartTime = 0, avgSpeedBackgroundWorker = false;
+let displayTextArr = ["", "", "", "", ""];
 
 function init() {
     inputbox = document.getElementById("inputbox");
@@ -11,38 +12,63 @@ function init() {
     avgSpeedBox = document.getElementById("avgSpd");
     speedometerProgressbar = document.getElementById("speedbox-score");
     queueSizeBox = document.getElementById("queueSize");
+    displayTextBox = document.getElementById("displayText");
 
     inputbox.oninput = inputboxHandler;
+
+    document.body.addEventListener("click", (event) => {
+        if (event.target !== event.currentTarget) return;
+        handleBodyClick();
+    });
+
     document.body.addEventListener('keyup', (event) => {
         if (event.target !== event.currentTarget) return;
+        if (!listening) return;
+        event.preventDefault();
+        document.body.focus();
         let char = event.key;
         if (char !== "Shift" && char !== "Alt" && char !== "Control") return;
         if (modifierKeyDown[char] === false) return;
         modifierKeyDown[char] = false;
         char += "Up";
-        event.preventDefault();
         console.log("Released key in body: ", char);
-        requestSend(char);
+        requestSend([char]);
     });
 
     document.body.addEventListener('keydown', (event) => {
         if (event.target !== event.currentTarget) return;
+        if (!listening) return;
+        event.preventDefault();
+        document.body.focus();
         let char = event.key;
         if (char === "Shift" || char === "Alt" || char === "Control") {
             if (modifierKeyDown[char] === true) return;
             modifierKeyDown[char] = true;
             char += "Down";
-            event.preventDefault();
         }
         console.log("Pressed key in body: ", char);
-        requestSend(char);
+        requestSend([char]);
     });
-    updateConnectionStatus();
+    updateConnectionStatusLabel(false);
+    requestUpdateConnectionStatus();
     setInterval(() => {
-        updateConnectionStatus();
-    }, 10000);
+        requestUpdateConnectionStatus();
+    }, 5000);
     updateAvgSpeedLabel(0);
     createAvgSpeedBackgroundWorker();
+}
+
+function onConnect() {
+    requestSend(["ShiftUp", "AltUp", "ControlUp"]);
+    if (requestListening) {
+        listening = true;
+        requestListening = false;
+    }
+}
+
+function onDisconnect() {
+    clearSendQueue();
+    listening = false;
 }
 
 function clearSendQueue() {
@@ -59,39 +85,90 @@ function removeAvgSpeedBackgroundWorker() {
     avgSpeedBackgroundWorker = false;
 }
 
+function handleConnectButton() {
+    console.log("Clicked connection button");
+    requestUpdateConnectionStatus();
+    if (!connected)
+        requestListening = true;
+    else
+        listening = true;
+    console.log(connected, listening, requestListening);
+    updateConnectionStatusLabel(connected);
+}
+
 function handleBodyClick() {
     console.log("Clicked body");
-    updateConnectionStatus();
+    requestUpdateConnectionStatus();
+    listening = false;
+    requestListening = false;
+    updateConnectionStatusLabel(connected);
 }
 
-function updateConnectionStatus() {
-    if (sleeping) requestSend("status");
+function requestUpdateConnectionStatus() {
+    if (sleeping) requestSend(["status"]);
 }
 
-function requestSend(code, isArr) {
-    // requestCount++;
-    if (isArr) {
-        sendQueue = sendQueue.concat(code);
-    } else {
-        sendQueue.push(code);
-    }
+function requestSend(code) {
+    sendQueue = sendQueue.concat(code);
+    addToDisplayText(code);
     if (sleeping) {
         handleStringArrInput();
     }
     updateQueueSizeLabel();
 }
 
+function addToDisplayText(arr) {
+    let numLines = displayTextArr.length;
+    let line = displayTextArr.pop();
+    while (arr.length > 0) {
+        while (line.length < 50 && arr.length > 0) {
+            if (arr[0] === "status") {
+                arr.shift();
+                continue;
+            }
+            line += arr.shift();
+        }
+        displayTextArr.push(line);
+        line = "";
+    }
+    let reanimate = displayTextArr.length !== numLines;
+    while (displayTextArr.length > 5) {
+        displayTextArr.shift();
+    }
+    updateDisplayText(reanimate);
+}
+
+function updateDisplayText(reanimate) {
+    let str = displayTextArr.join("<br>");
+    displayTextBox.innerHTML = str;
+    if (!reanimate) return;
+    displayTextBox.style.animation = 'none';
+    displayTextBox.offsetHeight; /* trigger reflow */
+    displayTextBox.style.animation = null;
+}
+
 function updateQueueSizeLabel() {
     queueSizeBox.innerHTML = "Clear Queue [" + sendQueue.length + "]";
 }
     
-function updateStatusLabel(connected) {
-    if(connected){
-        statusBox.innerHTML = "Connected";
-        statusBox.style.color = "green";     
+function updateConnectionStatusLabel(newStatus) {
+    connected = newStatus;
+    if (connected) {
+        if (listening) {
+            statusBox.innerHTML = "Connected";
+            statusBox.style.color = "green";
+        } else {
+            statusBox.innerHTML = "Ready";
+            statusBox.style.color = "green";
+        }
     } else {
-        statusBox.innerHTML = "Disconnected";
-        statusBox.style.color = "red";
+        if (requestListening) {
+            statusBox.innerHTML = "Connecting";
+            statusBox.style.color = "red";
+        } else {
+            statusBox.innerHTML = "Disconnected";
+            statusBox.style.color = "red";
+        }
     }
 }
 
@@ -153,7 +230,10 @@ function makeRequest(url, callback) {
     xmlHttp.timeout = 1000;
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4) {
-            updateStatusLabel(xmlHttp.status === 200);
+            let newStatus = xmlHttp.status === 200;
+            if (!connected && newStatus) onConnect();
+            if (connected && !newStatus) onDisconnect();
+            updateConnectionStatusLabel(newStatus);
             callback({ status: xmlHttp.status, res: xmlHttp.responseText });
         }
     }
